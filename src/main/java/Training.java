21d20179624bb1c961ea3867b981f3468de38c1f
LikeNoProblem <swozxy@gmail.com>
2018-07-29 00:44:30 +0300

    My Third Commit

--- src/main/java/Training.java
+++ src/main/java/Training.java
@@ -39,6 +39,7 @@ public class Training {
         wait = new WebDriverWait(driver,5);
         System.out.print(((HasCapabilities)driver).getCapabilities());
         System.out.print("New");
+        System.out.print("New");
     }
 
 
